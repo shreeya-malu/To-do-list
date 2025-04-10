e164a64ef91557d3bfe605a94a9c807e0124b04d
shreeya-malu <shreycomp123@gmail.com>
2025-04-10 14:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,8 @@ async function startServer() {
   server.applyMiddleware({ app });
 
   app.listen({ port: 4000 }, () =>
-    console.log('🚀 Server ready at http://localhost:4000${server.graphqlPath}')
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+)
   );
 }
 
